4c300d19f259c34530fc1d302b0c9b8d3589f0b8
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2021-12-08 10:41:39 +0100

    blabla

---
+++ cmake-build-debug/CMakeFiles/07_matrice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "07_matrice.exe"
+  "07_matrice.exe.manifest"
+  "07_matrice.pdb"
+  "CMakeFiles/07_matrice.dir/main.cpp.obj"
+  "lib07_matrice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/07_matrice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
